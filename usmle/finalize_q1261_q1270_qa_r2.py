0db97449b082a8f50464f63d05a97e44b82a1bdc
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-06 12:03:43 +0200

    Bind Q1261-Q1270 FINAL materializer to repaired R3 candidate

---
+++ usmle/finalize_q1261_q1270_qa_r2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+import finalize_q1261_q1270_qa as f
+
+# Bind FINAL materialization to the repaired R3 candidate blob.
+f.CAND_BLOB='25d7f71b6aa7866e5c5a8f0da7c7c9bc28e309ef'
+
+if __name__=='__main__':
+    f.main()
