b3e47be7dc0cc18871f17fa57ccfa156a7e5fe86
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-13 17:41:59 +0200

    usmle: add Q1579 zuranolone module

---
+++ usmle/q1579_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1579,'zuranolone','Nervous System & Special Senses',['Pharmacology','Neuroscience'],
+ 'A patient with postpartum depression receives a 14-day course of zuranolone. Shortly after dosing, central inhibitory neurotransmission is enhanced and psychomotor performance may be impaired.',
+ 'Which receptor is directly positively modulated by this drug?',
+ {'A':'NMDA glutamate receptor','B':'GABA-B receptor','C':'5-HT1A receptor','D':'GABA-A receptor','E':'Dopamine D2 receptor'},
+ 'D','Positive allosteric modulation of GABA-A receptors',
+ 'Zuranolone is a neuroactive steroid whose therapeutic action in postpartum depression is thought to be related to positive allosteric modulation of GABA-A receptors, thereby enhancing inhibitory GABAergic signaling.',
+ {'A':'NMDA receptors mediate excitatory glutamatergic transmission and are not the direct target.','B':'GABA-B receptors are metabotropic and are not the labeled receptor target of zuranolone.','C':'5-HT1A agonism is a serotonergic mechanism used by other agents.','D':'Correct. Zuranolone positively modulates GABA-A receptors.','E':'D2 antagonism is not the mechanism of zuranolone.'},
+ 'Recognize neuroactive-steroid positive allosteric modulation of GABA-A receptors as the direct pharmacologic action of zuranolone.',
+ 'ZURZUVAE- zuranolone capsule','f18e53b0-d0bb-422d-8de7-ab64b7292b29','B',
+ 'GABA-B receptor activation also enhances inhibition, but zuranolone specifically acts through positive allosteric modulation of GABA-A receptors.')
