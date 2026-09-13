61aa524bdce5497500107a23c6887f330ba5adc1
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-13 17:41:25 +0200

    usmle: add Q1576 ensifentrine module

---
+++ usmle/q1576_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1576,'ensifentrine','Respiratory System',['Pharmacology','Biochemistry'],
+ 'Airway smooth-muscle cells are exposed to ensifentrine. Intracellular cyclic nucleotide concentrations rise despite unchanged adenylyl cyclase and guanylyl cyclase activity.',
+ 'Which direct enzymatic action best explains this finding?',
+ {'A':'Inhibition of phosphodiesterases 3 and 4','B':'Activation of soluble guanylyl cyclase','C':'Inhibition of phosphodiesterase 5 only','D':'Activation of adenylyl cyclase','E':'Inhibition of protein kinase A'},
+ 'A','Dual inhibition of PDE3 and PDE4 with accumulation of intracellular cyclic nucleotides',
+ 'Ensifentrine directly inhibits PDE3 and PDE4. PDE3 hydrolyzes cAMP and can also hydrolyze cGMP, whereas PDE4 hydrolyzes cAMP; inhibition therefore increases intracellular cAMP and/or cGMP.',
+ {'A':'Correct. Ensifentrine inhibits PDE3 and PDE4.','B':'Guanylyl cyclase activation would increase cGMP synthesis rather than reduce cyclic-nucleotide breakdown.','C':'PDE5 selectively regulates cGMP and is not the direct target pair of ensifentrine.','D':'Adenylyl cyclase activation increases cAMP synthesis but is not ensifentrine pharmacology.','E':'Protein kinase A is downstream of cAMP and is not directly inhibited by ensifentrine.'},
+ 'Recognize dual PDE3/PDE4 inhibition as a mechanism that raises intracellular cyclic nucleotides in COPD therapy.',
+ 'OHTUVAYRE- ensifentrine inhalation suspension','e6433c98-41a0-4f99-8f7f-d0e9db6e3f40','C',
+ 'PDE5 inhibition is another phosphodiesterase-based mechanism, but it selectively targets cGMP metabolism and does not explain the labeled PDE3/PDE4 target profile of ensifentrine.')
