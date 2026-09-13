683d44d2b1ce8132181a7f97afe809a93fd78973
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-14 01:28:19 +0200

    usmle: add Q1587 mitapivat module

---
+++ usmle/q1587_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1587,'mitapivat','Blood & Lymphoreticular/Immune Systems',['Pharmacology','Biochemistry'],
+ 'Red blood cells from an adult with pyruvate kinase deficiency have low ATP and shortened survival. After mitapivat treatment, ATP production increases and hemolysis improves.',
+ 'Which molecular action most directly explains this response?',
+ {'A':'Inhibition of phosphofructokinase-1','B':'Allosteric activation of the pyruvate kinase tetramer','C':'Activation of glucose-6-phosphate dehydrogenase','D':'Inhibition of lactate dehydrogenase','E':'Activation of hexokinase'},
+ 'B','Allosteric activation of red-cell pyruvate kinase',
+ 'Mitapivat allosterically binds the pyruvate kinase tetramer and increases pyruvate kinase activity. In pyruvate kinase deficiency, reduced PK-R activity lowers ATP production and shortens red-cell lifespan; activation of the residual enzyme improves RBC energetics.',
+ {'A':'PFK-1 inhibition would reduce glycolytic flux and ATP generation.','B':'Correct. Mitapivat allosterically activates the pyruvate kinase tetramer.','C':'G6PD supports NADPH generation rather than the direct ATP-producing step targeted by mitapivat.','D':'Lactate dehydrogenase is downstream of pyruvate kinase and is not the direct target.','E':'Hexokinase is an upstream glycolytic enzyme and is not directly activated by mitapivat.'},
+ 'Connect allosteric pyruvate-kinase activation with increased RBC ATP production and improved survival in pyruvate kinase deficiency.',
+ 'PYRUKYND- mitapivat tablet, film coated; PYRUKYND- mitapivat kit','4ccee896-e313-4c6c-9674-b4746ada0a90','C',
+ 'G6PD activation could improve red-cell antioxidant capacity, but mitapivat specifically restores glycolytic ATP generation by activating pyruvate kinase.')
