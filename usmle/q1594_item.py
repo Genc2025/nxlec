59f76a7d2568810cb40befc0f43a57e9ceb92080
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-14 19:54:27 +0200

    usmle: add Q1594 nadofaragene module

---
+++ usmle/q1594_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1594,'nadofaragene firadenovec-vncg','Renal & Urinary System',['Pharmacology','Molecular Biology'],
+ 'A patient with BCG-unresponsive non-muscle-invasive bladder cancer receives an intravesical non-replicating adenoviral vector. Several days later, interferon-alfa 2b protein is detectable in the urine.',
+ 'Which event most directly explains this finding?',
+ {'A':'Selective lysis of urothelial cells by replicating adenovirus','B':'Systemic secretion of interferon by hepatocytes','C':'Activation of endogenous bladder interferon genes by CRISPR editing','D':'Transduction of urothelial cells with transient local expression of an interferon-alfa 2b transgene','E':'Release of interferon from intravesical BCG organisms'},
+ 'D','Adenoviral transduction of bladder urothelium with transient local IFN-alfa 2b expression',
+ 'Nadofaragene firadenovec-vncg is a non-replicating adenoviral vector designed to deliver a human interferon-alfa 2b gene to bladder urothelial cells. Intravesical administration produces local cell transduction and transient expression of IFN-alfa 2b.',
+ {'A':'The vector is non-replicating and is not intended to kill cells through viral replication.','B':'Expression is localized to the bladder urothelium rather than driven by hepatic secretion.','C':'The therapy delivers a transgene and does not edit the endogenous interferon locus with CRISPR.','D':'Correct. Urothelial transduction produces transient local IFN-alfa 2b expression.','E':'BCG is not the source of the detected interferon protein after this gene therapy.'},
+ 'Differentiate local adenoviral transgene expression from replicating viral therapy, systemic cytokine delivery, and genome editing.',
+ 'ADSTILADRIN- nadofaragene firadenovec-vncg suspension','5ca20ed4-da4f-463c-ac2a-ccd6ab3774fa','B',
+ 'Oncolytic viral replication could also produce local antitumor effects, but this vector is explicitly non-replicating and works through transient interferon transgene expression.')
