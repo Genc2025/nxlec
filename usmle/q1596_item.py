7b7ba3f75c9cd8f3358a36e977c0deabeabe405a
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-15 09:50:46 +0200

    usmle: add Q1596 deucravacitinib module

---
+++ usmle/q1596_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1596,'deucravacitinib','Musculoskeletal, Skin & Subcutaneous Tissue',['Pharmacology','Molecular Biology'],
+ 'A patient with plaque psoriasis receives a selective TYK2 inhibitor. In cell assays, receptor-mediated TYK2 activation falls even though the drug does not compete at the catalytic ATP-binding site.',
+ 'Which molecular interaction most directly explains this selectivity?',
+ {'A':'Binding to the TYK2 regulatory domain and stabilizing an inhibitory regulatory-catalytic interaction','B':'Covalent binding to the JAK1 catalytic cysteine','C':'Competitive occupation of the shared ATP-binding pocket of all JAK kinases','D':'Degradation of TYK2 mRNA through RNA interference','E':'Blockade of STAT DNA-binding domains'},
+ 'A','Allosteric inhibition by binding the TYK2 regulatory domain',
+ 'Deucravacitinib binds the regulatory domain of TYK2 and stabilizes an inhibitory interaction between its regulatory and catalytic domains. This produces allosteric inhibition of receptor-mediated TYK2 activation and downstream STAT signaling.',
+ {'A':'Correct. Deucravacitinib binds the TYK2 regulatory domain and allosterically suppresses catalytic activation.','B':'It does not act by covalently targeting JAK1.','C':'Its selectivity is not produced by competitive occupancy of the conserved catalytic ATP site.','D':'Deucravacitinib is not an RNA-interference therapy.','E':'STAT signaling decreases downstream, but STAT DNA-binding domains are not the direct drug target.'},
+ 'Distinguish regulatory-domain allosteric TYK2 inhibition from catalytic-site JAK inhibition and downstream STAT blockade.',
+ 'SOTYKTU- deucravacitinib tablet, film coated','ff4d7258-5068-4cdf-9692-8cae04c3198e','B',
+ 'JAK1 inhibition could also suppress cytokine signaling, but the defining selectivity of deucravacitinib is binding to the TYK2 regulatory domain rather than the catalytic ATP site.')
