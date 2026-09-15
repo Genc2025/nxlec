7a0e3d8b6617dd808a925b71d91ae059f19fc391
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-15 09:52:02 +0200

    usmle: add Q1600 voclosporin module

---
+++ usmle/q1600_item.py
@@ -0,0 +1,11 @@
+def build(make):
+ return make(1600,'voclosporin','Renal & Urinary System',['Pharmacology','Immunology'],
+ 'A patient with lupus nephritis receives voclosporin. T-cell cytokine production decreases after a calcium-dependent phosphatase pathway is suppressed.',
+ 'Which transcription factor remains relatively phosphorylated and therefore less able to activate T-cell gene expression?',
+ {'A':'NF-kappaB','B':'STAT3','C':'AP-1','D':'CREB','E':'NFAT'},
+ 'E','Calcineurin inhibition prevents NFAT dephosphorylation and activation',
+ 'Voclosporin is a calcineurin-inhibitor immunosuppressant. Activated calcineurin normally dephosphorylates nuclear factor of activated T-cell cytoplasmic (NFATc), promoting its transcriptional activity; calcineurin inhibition therefore suppresses lymphocyte proliferation and T-cell cytokine production.',
+ {'A':'NF-kappaB is an inflammatory transcription factor but is not the direct calcineurin substrate described here.','B':'STAT3 is activated primarily by JAK-mediated phosphorylation rather than calcineurin-dependent dephosphorylation.','C':'AP-1 participates in T-cell activation but is not the phosphatase target in this mechanism.','D':'CREB is regulated through kinase signaling and is not the relevant calcineurin substrate.','E':'Correct. Calcineurin inhibition prevents NFAT dephosphorylation and activation.'},
+ 'Link calcineurin inhibition with reduced NFAT activation and decreased T-cell cytokine transcription.',
+ 'LUPKYNIS- voclosporin capsule','9f489295-1156-52c7-5fd0-5c4c52f9b813','A',
+ 'NF-kappaB also controls inflammatory transcription, but voclosporin specifically suppresses the calcineurin-NFAT dephosphorylation pathway.')
