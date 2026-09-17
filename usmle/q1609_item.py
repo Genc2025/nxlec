e08ef8647f0d02fe83cc786e1c00b509a1e2e648
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-18 00:19:11 +0200

    usmle: author Q1609 item

---
+++ usmle/q1609_item.py
@@ -0,0 +1,32 @@
+def build(make_general):
+ return make_general(
+  1609,'','Cardiovascular System',['Biochemistry','Genetics','Physiology'],
+  'A 12-year-old boy has recurrent episodes of pancreatitis. Fasting plasma is grossly lipemic, with severe hypertriglyceridemia and persistent chylomicrons. Lipoprotein lipase protein is present and catalytically intact. In vitro, triglyceride hydrolysis by the patient\'s lipoproteins is restored when purified apolipoprotein C-II is added.',
+  'Which normal function is deficient in this patient?',
+  {
+   'A':'Assembly of apoB-containing lipoproteins by microsomal triglyceride transfer protein',
+   'B':'Hepatic uptake of chylomicron remnants through apoE-dependent receptors',
+   'C':'Transport and anchoring of lipoprotein lipase on capillary endothelium by GPIHBP1',
+   'D':'Cofactor activation of lipoprotein lipase by apolipoprotein C-II',
+   'E':'Inhibition of lipoprotein lipase by apolipoprotein C-III'
+  },
+  'D',
+  'Apolipoprotein C-II is an essential cofactor that activates lipoprotein lipase',
+  'Apolipoprotein C-II is present on triglyceride-rich lipoproteins and HDL and acts as a key cofactor and activator of lipoprotein lipase. APOC2 deficiency therefore causes familial chylomicronemia despite the presence of structurally intact LPL; adding apoC-II restores LPL-mediated triglyceride hydrolysis.',
+  {
+   'A':'MTP is required for assembly of apoB-containing lipoproteins; its deficiency decreases secretion of chylomicrons rather than causing persistent chylomicronemia with intact LPL.',
+   'B':'ApoE mediates remnant uptake after triglyceride hydrolysis; failure of remnant clearance does not explain restoration of lipolysis by apoC-II.',
+   'C':'GPIHBP1 is required to transport and anchor LPL at the capillary surface and can cause familial chylomicronemia, but the experiment shows that adding apoC-II restores activity when LPL itself is present and catalytically intact.',
+   'D':'Correct. ApoC-II is a required cofactor and activator of LPL, so its absence causes severe impairment of triglyceride hydrolysis.',
+   'E':'ApoC-III inhibits rather than activates LPL-mediated triglyceride clearance; loss of apoC-III would not produce this phenotype.'
+  },
+  'Distinguish APOC2 deficiency from LPL, GPIHBP1, and remnant-clearance disorders by recognizing apoC-II as a required activator of lipoprotein lipase.',
+  [{
+   'source_id':'Q1609-ENDOTEXT','title':'Genetics and Dyslipidemia - Endotext - NCBI Bookshelf',
+   'agency':'National Center for Biotechnology Information (NCBI), National Library of Medicine, NIH',
+   'url':'https://www.ncbi.nlm.nih.gov/books/NBK395584/','source_section_date':'2023-04-17',
+   'date_basis':'Endotext reports Last Update: 2023-04-17; live NCBI Bookshelf page reverified 2026-09-18.',
+   'retrieved_at':'2026-09-18','section_locator':'Apolipoprotein C-II (APOC2) Mutations'
+  }],
+  'C','GPIHBP1 deficiency can also cause familial chylomicronemia with reduced effective LPL activity at the endothelial surface, but direct restoration of lipolysis by purified apoC-II identifies loss of the LPL cofactor rather than its endothelial transporter.'
+ )
