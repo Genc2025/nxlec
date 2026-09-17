16d755d601f1e1f0af1ee592d11b3ca7132d0439
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-18 00:19:08 +0200

    usmle: author Q1608 item

---
+++ usmle/q1608_item.py
@@ -0,0 +1,32 @@
+def build(make_general):
+ return make_general(
+  1608,'','Gastrointestinal System',['Biochemistry','Genetics'],
+  'An infant has recurrent hypoglycemia, chronic vomiting and diarrhea, edema from protein-losing enteropathy, and hepatic dysfunction with coagulopathy. Neurodevelopment is age appropriate. Serum transferrin has an abnormal N-glycosylation pattern, and the clinical abnormalities improve substantially with oral mannose.',
+  'Deficiency of which biochemical reaction best explains this disorder?',
+  {
+   'A':'Glucose-6-phosphate to fructose-6-phosphate',
+   'B':'Mannose-6-phosphate to mannose-1-phosphate',
+   'C':'Fructose-6-phosphate to mannose-6-phosphate',
+   'D':'Mannose-1-phosphate plus GTP to GDP-mannose',
+   'E':'Addition of the mannose-6-phosphate targeting signal to lysosomal hydrolases'
+  },
+  'C',
+  'Mannose phosphate isomerase deficiency impairs interconversion of fructose-6-phosphate and mannose-6-phosphate',
+  'MPI-CDG is caused by deficiency of mannose phosphate isomerase, which interconverts fructose-6-phosphate and mannose-6-phosphate. Reduced endogenous mannose-6-phosphate limits mannose derivatives needed for glycosylation. Oral mannose can bypass the impaired isomerase step because hexokinase can phosphorylate mannose directly to mannose-6-phosphate.',
+  {
+   'A':'Phosphoglucose isomerase interconverts glucose-6-phosphate and fructose-6-phosphate; that reaction is upstream and does not explain the mannose-responsive glycosylation disorder.',
+   'B':'PMM2 converts mannose-6-phosphate to mannose-1-phosphate. PMM2-CDG commonly has prominent neurologic manifestations and is not bypassed by oral mannose in the same way as MPI-CDG.',
+   'C':'Correct. Mannose phosphate isomerase interconverts fructose-6-phosphate and mannose-6-phosphate; mannose supplementation bypasses the deficient step by supplying substrate that can be phosphorylated directly.',
+   'D':'GDP-mannose synthesis occurs downstream of mannose-1-phosphate; the vignette specifically reflects a defect that oral mannose can bypass at the mannose-6-phosphate level.',
+   'E':'Failure to add mannose-6-phosphate targeting tags causes I-cell disease with lysosomal hydrolase missorting, a different use of mannose-6-phosphate from N-glycan precursor synthesis.'
+  },
+  'Differentiate MPI-CDG from PMM2-CDG by identifying the fructose-6-phosphate ↔ mannose-6-phosphate reaction and understanding why oral mannose can bypass MPI deficiency.',
+  [{
+   'source_id':'Q1608-GLYCO','title':'Congenital Disorders of Glycosylation - Essentials of Glycobiology - NCBI Bookshelf',
+   'agency':'National Center for Biotechnology Information (NCBI), National Library of Medicine, NIH',
+   'url':'https://www.ncbi.nlm.nih.gov/books/NBK579928/','source_section_date':'2022',
+   'date_basis':'Essentials of Glycobiology 4th edition chapter published 2022; live NCBI Bookshelf page reverified 2026-09-18.',
+   'retrieved_at':'2026-09-18','section_locator':'DEFECTS IN N-GLYCAN BIOSYNTHESIS'
+  }],
+  'B','PMM2 deficiency affects the adjacent mannose-6-phosphate-to-mannose-1-phosphate step and also causes a congenital disorder of glycosylation, but the gastrointestinal/hepatic phenotype with preserved neurodevelopment and response to oral mannose specifically supports MPI deficiency.'
+ )
