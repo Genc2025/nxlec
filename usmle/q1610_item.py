56c3024d21771a7648d1cba26f7cb91d18e1e86e
Genc2025 <gencgrajqevci23@gmail.com>
2026-09-18 00:19:13 +0200

    usmle: author Q1610 item

---
+++ usmle/q1610_item.py
@@ -0,0 +1,41 @@
+def build(make_general):
+ return make_general(
+  1610,'','Blood & Lymphoreticular/Immune Systems',['Hematology','Physiology','Genetics'],
+  'A 20-year-old woman has recurrent delayed bleeding several hours after dental procedures and minor surgery. Platelet count, prothrombin time, activated partial thromboplastin time, platelet-function testing, and von Willebrand factor studies are normal. A clot forms normally ex vivo but undergoes abnormally rapid fibrinolysis, with persistent plasmin activity.',
+  'Loss of which physiologic action most directly explains this finding?',
+  {
+   'A':'Covalent cross-linking of fibrin by activated factor XIII',
+   'B':'Inhibition of tissue plasminogen activator and urokinase by plasminogen activator inhibitor 1',
+   'C':'Neutralization of thrombin by antithrombin',
+   'D':'Platelet adhesion mediated by von Willebrand factor binding to GPIb',
+   'E':'Direct inhibition of plasmin by alpha-2-antiplasmin'
+  },
+  'E',
+  'SERPINF2-encoded alpha-2-antiplasmin directly inhibits plasmin and restrains fibrin degradation',
+  'Alpha-2-antiplasmin, encoded by SERPINF2, is a major physiologic inhibitor of plasmin. Severe deficiency permits ongoing plasmin-mediated fibrin degradation after a clot has formed, producing premature clot breakdown and a bleeding disorder that can escape routine coagulation screening.',
+  {
+   'A':'Factor XIII stabilizes fibrin by covalent cross-linking, and severe deficiency can also cause delayed bleeding with normal PT and aPTT; however, it does not directly neutralize the persistent plasmin activity specified in the vignette.',
+   'B':'PAI-1 suppresses fibrinolysis upstream by inhibiting plasminogen activators such as tPA and urokinase; it does not directly inhibit plasmin itself.',
+   'C':'Antithrombin inhibits thrombin and other coagulation proteases. Loss predisposes to thrombosis rather than isolated rapid fibrin breakdown from persistent plasmin.',
+   'D':'vWF-mediated platelet adhesion affects primary hemostasis, whereas this patient forms a clot normally and then lyses it too rapidly; vWF testing is also normal.',
+   'E':'Correct. Alpha-2-antiplasmin directly binds and inhibits plasmin, limiting fibrin degradation after clot formation.'
+  },
+  'Differentiate alpha-2-antiplasmin deficiency from factor XIII and PAI-1 defects by identifying alpha-2-antiplasmin as the direct physiologic inhibitor of plasmin.',
+  [
+   {
+    'source_id':'Q1610-NCBI-GENE','title':'SERPINF2 serpin family F member 2 [Homo sapiens (human)] - Gene - NCBI',
+    'agency':'National Center for Biotechnology Information (NCBI), National Library of Medicine, NIH',
+    'url':'https://www.ncbi.nlm.nih.gov/gene/5345','source_section_date':'2026-07-06',
+    'date_basis':'NCBI Gene record updated 2026-07-06; live page reverified 2026-09-18.',
+    'retrieved_at':'2026-09-18','section_locator':'Summary'
+   },
+   {
+    'source_id':'Q1610-PMC','title':'Bleeding Disorders in Primary Fibrinolysis - PubMed Central',
+    'agency':'PubMed Central, National Library of Medicine, NIH',
+    'url':'https://pmc.ncbi.nlm.nih.gov/articles/PMC8268566/','source_section_date':'2021',
+    'date_basis':'Peer-reviewed review published 2021; live PubMed Central article reverified 2026-09-18.',
+    'retrieved_at':'2026-09-18','section_locator':'antiplasmin deficiency'
+   }
+  ],
+  'B','PAI-1 deficiency is also a primary hyperfibrinolytic disorder and can present with normal routine coagulation studies, but PAI-1 acts upstream on plasminogen activators. Persistent plasmin activity is most directly explained by loss of alpha-2-antiplasmin.'
+ )
